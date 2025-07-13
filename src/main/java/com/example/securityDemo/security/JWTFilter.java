e336c955e28ee7b2dc9bf399fad06b3293cd892f
test1234d5x2 <moselsh1@gmail.com>
2025-07-13 19:47:19 +0100

    Minor Issue Fixed

--- src/main/java/com/example/securityDemo/security/JWTFilter.java
+++ src/main/java/com/example/securityDemo/security/JWTFilter.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.example.securityDemo.models.User;
@@ -18,6 +19,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JWTFilter extends OncePerRequestFilter {
 
     @Autowired
